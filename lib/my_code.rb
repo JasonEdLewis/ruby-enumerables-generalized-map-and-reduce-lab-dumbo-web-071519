d7b353d11a7feb855f12d1b21602de0cf8028b6f
Jason Lewis <jasonedlewis@gmail.com>
2019-07-05 21:26:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,8 +19,8 @@ def reduce(array,start = nil)
     end
     accumulator = start
     while i < array.length
-    current = yield (array[i], accumulator)
-    accumulator += current
+    current = array[i]
+    accumulator += yield(current, accumulator)
     i += 1
   end
     accumulator
