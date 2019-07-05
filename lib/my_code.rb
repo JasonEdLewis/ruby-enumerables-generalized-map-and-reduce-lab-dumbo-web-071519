189c39e93544e438495bb04e58b9453fe51dc298
Jason Lewis <jasonedlewis@gmail.com>
2019-07-05 21:01:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+def map(arr)
+  results = []
+  i = 0
+  while i < arr.length
+  n = yield arr[i]
+  results.push(n)
+  i += 1
+end
+  results
+end
+
+def reduce(array,start = 0)
+   i = 0
+    if start == ""
+     start = array[0]
+    else
+    accumulator = yield start
+    while i < array.length
+    current = yield array[i]
+    accumulator += current
+    i += 1
+    end
+  end
+    accumulator
+end
+
